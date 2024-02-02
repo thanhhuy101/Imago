b6563e1a7f7f4eb169cf3b75fa6f08792da0ac12
Soodenne <ANH.NT13147@sinhvien.hoasen.edu.vn>
2024-02-02 10:54:41 +0700

    create navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { NgClass } from "@angular/common";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor(private router: Router) { }
+
+  itemSelected = 0;
+  navBarItems = [
+    { icon: 'desktop_windows', path: '/home' },
+    { icon: 'search', path: '' },
+    { icon: 'edit_square', path: '/creator' },
+    { icon: 'local_fire_department', path: '' },
+    { icon: 'for_you', path: '/profile' }
+  ];
+
+  select(i: number) {
+    this.itemSelected = i;
+    this.router.navigate([this.navBarItems[i].path]);
+  }
 
 }
