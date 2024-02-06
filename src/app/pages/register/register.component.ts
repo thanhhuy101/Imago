8415a429c58cc7279f9417e6c1279cf5bfcbe385
Dazzie2705 <Daztegnh2705@gmail.com>
2024-02-06 21:00:03 +0700

    loginregisterITSS

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {TaigaModule} from "../../shared/taiga.module";
+import {AsyncPipe} from "@angular/common";
+import {ReactiveFormsModule,FormControl , FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [TaigaModule, AsyncPipe, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RegisterComponent {
+  readonly registForm = new FormGroup({
+    userName: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    checkBox: new FormControl( false),
 
+  });
 }
