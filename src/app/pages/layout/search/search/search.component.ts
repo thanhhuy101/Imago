1809fa00319c7f5274fe11928965a3ead99f629a
lekhoi2208 <lekhoi2208@gmail.com>
2024-02-21 13:52:26 +0700

    profile & search

---
+++ src/app/pages/layout/search/search/search.component.ts
@@ -0,0 +1,61 @@
+import {Component, Inject} from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {TuiTabsModule} from "@taiga-ui/kit";
+import {Router, RouterOutlet} from "@angular/router";
+import {TuiAlertService} from "@taiga-ui/core";
+import {TaigaModule} from "../../../../shared/taiga.module";
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [
+    NgForOf,
+    TuiTabsModule,
+    RouterOutlet,
+    TaigaModule,
+  ],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss'
+})
+export class SearchComponent {
+  readonly items = [
+    {
+      text: 'Post',
+      router: '/post',
+      icon: 'tuiIconFileText',
+    },
+    {
+      text: 'People',
+      router: '/people',
+      icon: 'tuiIconUser',
+    },
+    {
+      text: 'Group',
+      router: '/group',
+      icon: 'tuiIconUsers',
+    },
+
+    {
+      text: 'Explore',
+      router: '/explore',
+      icon: 'tuiIconCompassLarge',
+    },
+
+  ];
+
+  activeItemIndex = 0;
+
+  constructor(@Inject(TuiAlertService) private readonly alerts: TuiAlertService,private route:Router) {}
+
+
+  onActiveItemChange(index: number) {
+
+    this.onChangePage(index)
+  }
+
+  onChangePage(i:number){
+    console.log(this.items[i].router);
+    this.route.navigate(['/search'+this.items[i].router]);
+
+  }
+}
