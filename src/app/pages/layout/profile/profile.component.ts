4d5676fba417b77234b6466ca5a21a70ea0fa446
Thanh Thien <thien.mlt8601@sinhvien.hoasen.edu.vn>
2024-03-01 11:53:33 +0700

    .

--- src/app/pages/layout/profile/profile.component.ts
+++ src/app/pages/layout/profile/profile.component.ts
@@ -1,15 +1,15 @@
-import {Component, Inject} from '@angular/core';
-import {TaigaModule} from "../../../shared/taiga.module";
-import {ShareModule} from "../../../shared/share.module";
-import {TuiAlertService} from "@taiga-ui/core";
-import {Router, RouterOutlet} from "@angular/router";
+import { Component, Inject } from '@angular/core';
+import { TaigaModule } from '../../../shared/taiga.module';
+import { ShareModule } from '../../../shared/share.module';
+import { TuiAlertService } from '@taiga-ui/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [TaigaModule, ShareModule, RouterOutlet],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
   readonly items = [
@@ -29,17 +29,25 @@ export class ProfileComponent {
 
   activeItemIndex = 0;
 
-  constructor(@Inject(TuiAlertService) private readonly alerts: TuiAlertService,private route:Router) {}
-
-
-  onActiveItemChange(index: number) {
-
-    this.onChangePage(index)
+  constructor(
+    @Inject(TuiAlertService) private readonly alerts: TuiAlertService,
+    private route: Router,
+  ) {
+    let path = window.location.href.split('?')[0];
+    console.log(path);
+    if (path.includes('profile/share')) {
+      this.activeItemIndex = 1;
+    } else if (path.includes('profile/mention')) {
+      this.activeItemIndex = 2;
+    }
   }
 
-  onChangePage(i:number){
-    console.log(this.items[i].router);
-    this.route.navigate(['/profile'+this.items[i].router]);
+  onActiveItemChange(index: number) {
+    this.onChangePage(index);
+  }
 
+  onChangePage(i: number) {
+    console.log(this.items[i].router);
+    this.route.navigate(['/profile' + this.items[i].router]);
   }
 }
--- src/app/pages/layout/search/search.component.ts
+++ src/app/pages/layout/search/search.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { TaigaModule } from '../../../shared/taiga.module';
 import { ShareModule } from '../../../shared/share.module';
-import { Router, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { TuiAlertService } from '@taiga-ui/core';
 import { FormControl } from '@angular/forms';
 
@@ -13,7 +13,6 @@ import { FormControl } from '@angular/forms';
   styleUrl: './search.component.scss',
 })
 export class SearchComponent implements OnInit {
-  activeItemIndex = 0;
   readonly items = [
     {
       text: 'Post',
@@ -36,33 +35,31 @@ export class SearchComponent implements OnInit {
       icon: 'tuiIconCompass',
     },
   ];
+  activeItemIndex = 0;
+
   searchValue = new FormControl();
   constructor(
     @Inject(TuiAlertService) private readonly alerts: TuiAlertService,
     private route: Router,
   ) {}
-
+  ngOnInit(): void {
+    // let path = window.location.href.split('?')[0];
+    // console.log(path);
+    // if (path.includes('search/people')) {
+    //   this.activeItemIndex = 1;
+    // } else if (path.includes('search/group')) {
+    //   this.activeItemIndex = 2;
+    // } else if (path.includes('search/explore')) {
+    //   this.activeItemIndex = 3;
+    // } else {
+    //   this.activeItemIndex = 0;
+    // }
+  }
   onActiveItemChange(index: number) {
     this.onChangePage(index);
   }
 
   onChangePage(i: number) {
-    console.log(this.items[i].router);
-    this.activeItemIndex = i;
     this.route.navigate(['/search' + this.items[i].router]);
-  }
-
-  ngOnInit(): void {
-    let path = window.location.href.split('?')[0];
-    console.log(path);
-    if (path.includes('search/people')) {
-      this.activeItemIndex = 1;
-    } else if (path.includes('search/group')) {
-      this.activeItemIndex = 2;
-    } else if (path.includes('search/explore')) {
-      this.activeItemIndex = 3;
-    } else {
-      this.activeItemIndex = 0;
-    }
   }
 }
