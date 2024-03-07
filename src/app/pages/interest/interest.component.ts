3454373602f2030eb67acfe4f1d6237ed6c9cc4f
bqtruong2000 <bqtruong2021@gmail.com>
2024-03-07 16:49:11 +0700

    creata category page

--- src/app/pages/interest/interest.component.ts
+++ src/app/pages/interest/interest.component.ts
@@ -10,6 +10,182 @@ import { TaigaModule } from '../../shared/taiga.module';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InterestComponent {
-  constructor() {}
+  //carousel
+  index = 0;
+  readonly itemsCount = 5;
+  readonly items = [
+    {
+      title: 'ART',
+      logo: 'Brush',
+      isActive: false,
+    },
+    {
+      title: 'DESIGN',
+      logo: 'Draw',
+      isActive: false,
 
+    },
+    {
+      title: 'PHOTOGRAPHY',
+      logo: 'photo_camera',
+      isActive: false,
+
+    },
+    {
+      title: 'TRAVEL',
+      logo: 'beach_access',
+      isActive: false,
+
+    },
+    {
+      title: 'FOOD',
+      logo: 'local_dining',
+      isActive: false,
+
+    },
+    {
+      title: 'FASHION',
+      logo: 'apparel',
+      isActive: false,
+
+    },
+    {
+      title: 'MUSIC',
+      logo: 'headphones',
+      isActive: false,
+
+    },
+    {
+      title: 'SPORT',
+      logo: 'sports_basketball',
+      isActive: false,
+
+    },
+    {
+      title: 'TECHNOLOGY',
+      logo: 'computer',
+      isActive: false,
+
+    },
+    {
+      title: 'HEALTH',
+      logo: 'ecg_heart',
+      isActive: false,
+
+    },
+    {
+      title: 'BEAUTY',
+      logo: 'health_and_beauty',
+      isActive: false,
+
+    },
+    {
+      title: 'CARS',
+      logo: 'directions_car',
+      isActive: false,
+    },
+
+  ];
+  readonly secondaryItems = [
+    {
+      title: 'ART',
+      logo: 'Brush',
+      isActive: false,
+    },
+    {
+      title: 'DESIGN',
+      logo: 'Draw',
+      isActive: false,
+
+    },
+    {
+      title: 'PHOTOGRAPHY',
+      logo: 'photo_camera',
+      isActive: false,
+
+    },
+    {
+      title: 'TRAVEL',
+      logo: 'beach_access',
+      isActive: false,
+
+    },
+    {
+      title: 'FOOD',
+      logo: 'local_dining',
+      isActive: false,
+
+    },
+    {
+      title: 'FASHION',
+      logo: 'apparel',
+      isActive: false,
+
+    },
+    {
+      title: 'MUSIC',
+      logo: 'headphones',
+      isActive: false,
+
+    },
+    {
+      title: 'SPORT',
+      logo: 'sports_basketball',
+      isActive: false,
+
+    },
+    {
+      title: 'TECHNOLOGY',
+      logo: 'computer',
+      isActive: false,
+
+    },
+    {
+      title: 'HEALTH',
+      logo: 'ecg_heart',
+      isActive: false,
+
+    },
+    {
+      title: 'BEAUTY',
+      logo: 'health_and_beauty',
+      isActive: false,
+
+    },
+    {
+      title: 'CARS',
+      logo: 'directions_car',
+      isActive: false,
+    },
+  ];
+
+  // items
+  selectedItems:any = [];
+  get rounded(): number {
+    return Math.floor(this.index / this.itemsCount);
+  }
+
+  onIndex(index: number): void {
+    this.index = index * this.itemsCount;
+  }
+
+  toggleActive(item: any) {
+    if (this.selectedItems.length < 5) {
+      item.isActive = !item.isActive;
+      if (item.isActive) {
+        this.selectedItems.push(item);
+      } else {
+        this.selectedItems = this.selectedItems.filter(
+          (i: any) => i.title !== item.title,
+        );
+      }
+    } else {
+      if (item.isActive) {
+        item.isActive = !item.isActive;
+        this.selectedItems = this.selectedItems.filter(
+          (i: any) => i.title !== item.title,
+        );
+      }
+    }
+  }
 }
